47615f73fcc8c59b4985c08fe49eb20d09b0a23d
hz <cnhuangzhou@163.com>
2017-02-06 15:00:51 +0800

    首次提交版本

--- app/src/main/java/com/hz/selectphoto/MainActivity.java
+++ app/src/main/java/com/hz/selectphoto/MainActivity.java
@@ -87,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
